588442e20f68b42a30c7a125a78ef3d50f543ccb
janith720 <janithukwattage@gmail.com>
2022-07-29 09:21:52 +0530

    developed movieDB

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import MovieCard from "./MovieCard";
+import NoMovies from "./NoMovies";
+import searchIcon from "./search.svg";
+
+//c4a16f13
+
+const apiUrl = "http://www.omdbapi.com?apikey=c4a16f13";
 
 function App() {
+  const [movies, setMovies] = React.useState([]);
+  const [searchText, setSearchText] = React.useState("");
+  const [error, setError] = React.useState(false);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+    console.log(data);
+  };
+
+  const handleSearch = () => {
+    if (searchText === "") {
+      setError(true)
+    }else{
+      searchMovies(searchText)
+      setError(false)
+    }
+  };
+
+  React.useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Land</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <img src={searchIcon} alt="search" onClick={handleSearch} />
+      </div>
+
+      {error && (
+        <div className="empty">
+          <NoMovies />
+        </div>
+      )}
+
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((val, key) => {
+            return <MovieCard key={key} movie1={val} />;
+          })}
+        </div>
+      ) : (
+        <div className="empty">
+          {/* <NoMovies /> */}
+        </div>
+      )}
+      
     </div>
   );
 }
---
+++ src/NoMovies.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NoMovies() {
+  return (
+    <div>
+        <h2>No movies found</h2>
+    </div>
+  )
+}
